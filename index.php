14dd541ea990c81a974b61af0890d296d44923e4
yancell <35684687+yancell@users.noreply.github.com>
2023-04-08 22:31:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, "_token=OTbouH1x5Xw2Gt2ISp5er1wOlWuFvBbrzsb
 $ch2 = curl_init();
 curl_setopt($ch2, CURLOPT_HEADER, false);
 curl_setopt($ch2, CURLOPT_NOBODY, false);
-curl_setopt($ch2, CURLOPT_URL, "https://dewafortune.xyz//auth/select_game_v2.php");
+curl_setopt($ch2, CURLOPT_URL, "https://dwvgs.biz/dewafortune/DAHYANI");
 curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch2, CURLOPT_COOKIEJAR, "cookie.txt");
 curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie.txt");
