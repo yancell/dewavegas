62dde7084fd9ac32bca9ec576f20e95604f58a7b
yancell <35684687+yancell@users.noreply.github.com>
2023-04-08 23:31:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "_token=cGjcbUlDaEhT15z0TUCe1OeFXzCsNd1jCUrbgRCw&username=DAHYANI&password=cikande1");
+curl_setopt($ch, CURLOPT_POSTFIELDS, "username=DAHYANI&password=cikande1");
 
 $ch2 = curl_init();
 curl_setopt($ch2, CURLOPT_HEADER, false);
