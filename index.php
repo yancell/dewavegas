2983f802fb7de83b46bb738ad6246983d66b254b
yancell <35684687+yancell@users.noreply.github.com>
2023-04-08 23:08:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,20 +18,25 @@ $ch2 = curl_init();
 curl_setopt($ch2, CURLOPT_HEADER, false);
 curl_setopt($ch2, CURLOPT_NOBODY, false);
 curl_setopt($ch2, CURLOPT_URL, "https://dwvgs.biz/dewafortune/DAHYANI");
-//curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, 0);
-//curl_setopt($ch2, CURLOPT_COOKIEJAR, "cookie.txt");
 curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie.txt");
 curl_setopt($ch2, CURLOPT_COOKIE, "cookie.txt");
-//curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, 0);
-//curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
-//curl_setopt($ch2, CURLOPT_POST, 1);
-//curl_setopt($ch2, CURLOPT_POSTFIELDS, "res=d1d813a48d99f0e102f7d0a1b9068001&resres=d1d813a48d99f0e102f7d0a1b9068001");
+
+$ch3 = curl_init();
+curl_setopt($ch3, CURLOPT_HEADER, false);
+curl_setopt($ch3, CURLOPT_NOBODY, false);
+curl_setopt($ch3, CURLOPT_URL, "https://dewafortune.xyz/bq2.php");
+curl_setopt($ch3, CURLOPT_COOKIEFILE, "cookie.txt");
+curl_setopt($ch3, CURLOPT_COOKIE, "cookie.txt");
+curl_setopt($ch3, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch3, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch3, CURLOPT_POST, 1);
+curl_setopt($ch3, CURLOPT_POSTFIELDS, "res=d1d813a48d99f0e102f7d0a1b9068001&resres=d1d813a48d99f0e102f7d0a1b9068001");
 
 $mh = curl_multi_init();
 curl_multi_add_handle($mh, $ch);
 curl_multi_add_handle($mh, $ch2);
+curl_multi_add_handle($mh, $ch3);
 $running = NULL;
 do {
   usleep(10000);
@@ -39,8 +44,11 @@ do {
 } while($running > 0);
 $a = curl_multi_getcontent($ch);
 $b = curl_multi_getcontent($ch2);
+$c = curl_multi_getcontent($ch3);
 curl_multi_remove_handle($mh, $ch);
 curl_multi_remove_handle($mh, $ch2);
+curl_multi_remove_handle($mh, $ch3);
 curl_multi_close($mh);
 echo $a;
 echo $b;
+echo $c;
