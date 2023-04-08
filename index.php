59858e902362a951847a0d06913ffd6f74dd3671
yancell <35684687+yancell@users.noreply.github.com>
2023-04-08 22:19:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,22 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "_token=ptOgvIUoj0pgLlQi7I66z62a4yDChYPcZVg8SS2V&username=DAHYANI&password=cikande1");
 
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_HEADER, false);
+curl_setopt($ch2, CURLOPT_NOBODY, false);
+curl_setopt($ch2, CURLOPT_URL, "https://dewafortune.xyz/bq2.php");
+curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie.txt");
+curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2, CURLOPT_POSTFIELDS, "res=d1d813a48d99f0e102f7d0a1b9068001&resres=d1d813a48d99f0e102f7d0a1b9068001");
+
 $mh = curl_multi_init();
 curl_multi_add_handle($mh, $ch);
+curl_multi_add_handle($mh, $ch2);
 $running = NULL;
 do {
   usleep(10000);
@@ -22,5 +36,6 @@ do {
 } while($running > 0);
 $a = curl_multi_getcontent($ch);
 curl_multi_remove_handle($mh, $ch);
+curl_multi_remove_handle($mh, $ch2);
 curl_multi_close($mh);
 echo $a;
