37fc77fa77ebe66bd47b4dd62b3f169d62577aa8
yancell <35684687+yancell@users.noreply.github.com>
2023-04-08 23:28:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ curl_setopt($ch3, CURLOPT_COOKIE, "cookie.txt");
 curl_setopt($ch3, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch3, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch3, CURLOPT_POST, 1);
-//curl_setopt($ch3, CURLOPT_POSTFIELDS, "res=d1d813a48d99f0e102f7d0a1b9068001&resres=d1d813a48d99f0e102f7d0a1b9068001");
+curl_setopt($ch3, CURLOPT_POSTFIELDS, "resres=bda9643ac6601722a28f238714274da4&res=bda9643ac6601722a28f238714274da4");
 
 $mh = curl_multi_init();
 curl_multi_add_handle($mh, $ch);
