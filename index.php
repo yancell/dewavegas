c9b3366693cb756ff89a63678b3053c5a3e3352c
yancell <35684687+yancell@users.noreply.github.com>
2023-04-09 09:47:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "username=DAHYANI&password=cikande1");
 curl_setopt($ch, CURLOPT_VERBOSE, 1);
-**/
+
 $ch2 = curl_init();
 curl_setopt($ch2, CURLOPT_HEADER, 1);
 curl_setopt($ch2, CURLOPT_NOBODY, 1);
@@ -23,7 +23,7 @@ curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie.txt");
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
 curl_setopt($ch2, CURLOPT_VERBOSE, 1);
-/**
+
 $ch3 = curl_init();
 curl_setopt($ch3, CURLOPT_HEADER, false);
 curl_setopt($ch3, CURLOPT_NOBODY, false);
@@ -40,22 +40,36 @@ curl_setopt($ch3, CURLOPT_FOLLOWLOCATION, 0);
 
 curl_setopt($ch3, CURLOPT_VERBOSE, 1);
 **/
+$ch4 = curl_init();
+curl_setopt($ch4, CURLOPT_HEADER, 1);
+curl_setopt($ch4, CURLOPT_NOBODY, 1);
+curl_setopt($ch4, CURLOPT_URL, "https://dewafortune.xyz/auth/login_defor.php?userid=DAHYANI&sessid=dqf7JfTEW5W5X7hfXquqZBFzQifVfXFIwbl1JeQm&access_token=805f6c37fa1eb7e50c2507cbe7128280&sesskey=202304093.236.66.167");
+curl_setopt($ch4, CURLOPT_COOKIEJAR, "cookie1.txt");
+curl_setopt($ch4, CURLOPT_COOKIEFILE, "cookie1.txt");
+curl_setopt($ch4, CURLOPT_FOLLOWLOCATION, 0);
+curl_setopt($ch4, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch4, CURLOPT_VERBOSE, 1);
+
 $mh = curl_multi_init();
 //curl_multi_add_handle($mh, $ch);
-curl_multi_add_handle($mh, $ch2);
+//curl_multi_add_handle($mh, $ch2);
 //cur№l_multi_add_handle($mh, $ch3);
+curl_multi_add_handle($mh, $ch4);
 $running = NULL;
 do {
   usleep(10000);
   curl_multi_exec($mh, $running);
 } while($running > 0);
 //$a = curl_multi_getcontent($ch);
-$b = curl_multi_getcontent($ch2);
+//$b = curl_multi_getcontent($ch2);
 //$c = curl_multi_getcontent($ch3);
+$d = curl_multi_getcontent($ch4);
 //curl_multi_remove_handle($mh, $ch);
-curl_multi_remove_handle($mh, $ch2);
+//curl_multi_remove_handle($mh, $ch2);
 //curl_multi_remove_handle($mh, $ch3);
+curl_multi_remove_handle($mh, $ch4);
 curl_multi_close($mh);
 //echo $a;
-echo $b;
+//echo $b;
 //echo $c;
+echo $d;
