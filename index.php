6385708b5852a64d5069f011ed77d1ec158bf4c8
yancell <35684687+yancell@users.noreply.github.com>
2023-04-09 11:32:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "username=DAHYANI&password=cikande1");
 curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_exec($ch);
+curl_close($ch);
 
 $ch2 = curl_init();
 curl_setopt($ch2, CURLOPT_HEADER, 1);
@@ -18,15 +20,7 @@ curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie.txt");
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
 curl_setopt($ch2, CURLOPT_VERBOSE, 1);
-
-$mh = curl_multi_init();
-curl_multi_add_handle($mh, $ch);
-curl_multi_add_handle($mh, $ch2);
-$running = NULL;
-do {
-  usleep(30000);
-  curl_multi_exec($mh, $running);
-} while($running > 0);
-curl_multi_remove_handle($mh, $ch);
-curl_multi_remove_handle($mh, $ch2);
-curl_multi_close($mh);
+curl_exec($ch2);
+$info = curl_getinfo($ch2, CURLINFO_REDIRECT_URL);
+curl_close($ch2);
+echo $info;
