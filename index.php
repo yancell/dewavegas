367cc3da44d34cc568036aa3fe38d85aa135f62f
yancell <35684687+yancell@users.noreply.github.com>
2023-04-09 11:57:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,6 @@ curl_setopt($ch2, CURLOPT_NOBODY, 1);
 curl_setopt($ch2, CURLOPT_URL, "https://dwvgs.club/dewafortune/DAHYANI");
 curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie.txt");
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 0);
 curl_setopt($ch2, CURLOPT_VERBOSE, 1);
 curl_exec($ch2);
 $info = curl_getinfo($ch2, CURLINFO_REDIRECT_URL);
@@ -32,6 +31,7 @@ curl_setopt($ch3, CURLOPT_COOKIEJAR, "cookie1.txt");
 curl_setopt($ch3, CURLOPT_COOKIEFILE, "cookie1.txt");
 curl_setopt($ch3, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch3, CURLOPT_VERBOSE, 1);
+curl_setopt($ch3, CURLOPT_FOLLOWLOCATION, 1);
 $cnt = curl_exec($ch3);
 curl_close($ch3);
 
