a63338699cfccf81e8e925b3b992ddffb9f4e5a4
yancell <35684687+yancell@users.noreply.github.com>
2023-04-09 09:00:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-//$ch = curl_init();
-//curl_setopt($ch, CURLOPT_HEADER, false);
-//curl_setopt($ch, CURLOPT_NOBODY, false);
-//curl_setopt($ch, CURLOPT_URL, "https://dwvgs.club/loginUser");
-//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-//curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie.txt");
-//curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
-//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-//curl_setopt($ch, CURLOPT_POST, 1);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, "username=DAHYANI&password=cikande1");
-//curl_setopt($ch, CURLOPT_VERBOSE, 1);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_NOBODY, false);
+curl_setopt($ch, CURLOPT_URL, "https://dwvgs.club/loginUser");
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie.txt");
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "username=DAHYANI&password=cikande1");
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
 
 $ch2 = curl_init();
 curl_setopt($ch2, CURLOPT_HEADER, false);
@@ -38,7 +38,7 @@ curl_setopt($ch3, CURLOPT_FOLLOWLOCATION, 0);
 curl_setopt($ch3, CURLOPT_VERBOSE, 1);
 
 $mh = curl_multi_init();
-//curl_multi_add_handle($mh, $ch);
+curl_multi_add_handle($mh, $ch);
 curl_multi_add_handle($mh, $ch2);
 curl_multi_add_handle($mh, $ch3);
 $running = NULL;
@@ -46,13 +46,13 @@ do {
   usleep(10000);
   curl_multi_exec($mh, $running);
 } while($running > 0);
-//$a = curl_multi_getcontent($ch);
+$a = curl_multi_getcontent($ch);
 $b = curl_multi_getcontent($ch2);
 $c = curl_multi_getcontent($ch3);
-//curl_multi_remove_handle($mh, $ch);
+curl_multi_remove_handle($mh, $ch);
 curl_multi_remove_handle($mh, $ch2);
 curl_multi_remove_handle($mh, $ch3);
 curl_multi_close($mh);
-//echo $a;
+echo $a;
 echo $b;
 echo $c;
