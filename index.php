1de6f7b21b39ed40b46c98e6f52fb389e03f6cdc
yancell <35684687+yancell@users.noreply.github.com>
2023-04-09 10:02:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ curl_setopt($ch3, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch3, CURLOPT_FOLLOWLOCATION, 0);
 
 curl_setopt($ch3, CURLOPT_VERBOSE, 1);
-
+**/
 $ch4 = curl_init();
 curl_setopt($ch4, CURLOPT_HEADER, 1);
 curl_setopt($ch4, CURLOPT_NOBODY, 1);
@@ -49,7 +49,7 @@ curl_setopt($ch4, CURLOPT_COOKIEFILE, "cookie1.txt");
 curl_setopt($ch4, CURLOPT_FOLLOWLOCATION, 0);
 curl_setopt($ch4, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch4, CURLOPT_VERBOSE, 1);
-**/
+
 $ch5 = curl_init();
 curl_setopt($ch5, CURLOPT_HEADER, 1);
 curl_setopt($ch5, CURLOPT_NOBODY, 1);
@@ -64,7 +64,7 @@ $mh = curl_multi_init();
 //curl_multi_add_handle($mh, $ch);
 //curl_multi_add_handle($mh, $ch2);
 //cur№l_multi_add_handle($mh, $ch3);
-//curl_multi_add_handle($mh, $ch4);
+curl_multi_add_handle($mh, $ch4);
 curl_multi_add_handle($mh, $ch5);
 $running = NULL;
 do {
@@ -74,7 +74,7 @@ do {
 //$a = curl_multi_getcontent($ch);
 //$b = curl_multi_getcontent($ch2);
 //$c = curl_multi_getcontent($ch3);
-//$d = curl_multi_getcontent($ch4);
+$d = curl_multi_getcontent($ch4);
 $e = curl_multi_getcontent($ch5); 
 //curl_multi_remove_handle($mh, $ch);
 //curl_multi_remove_handle($mh, $ch2);
@@ -85,5 +85,5 @@ curl_multi_close($mh);
 //echo $a;
 //echo $b;
 //echo $c;
-//echo $d;
+echo $d;
 echo $e;
