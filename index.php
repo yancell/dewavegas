70503514abb785d70157d8784b99a4e587b7cc7e
yancell <35684687+yancell@users.noreply.github.com>
2023-04-10 23:21:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ curl_setopt($ch3, CURLOPT_FOLLOWLOCATION, 1);
 curl_exec($ch3);
 curl_close($ch3);
 
-sleep(20);
+sleep(10);
 
 $ch4 = curl_init();
 curl_setopt($ch4, CURLOPT_URL, "https://dewafortune.xyz/var_api.php");
