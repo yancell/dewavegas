870bf83d8241fb718825d4b7a592f7d9e9838ef9
yancell <35684687+yancell@users.noreply.github.com>
2023-04-15 07:03:54 +0700

    Update bq2.php

--- bq2.php
+++ bq2.php
@@ -1,8 +1,13 @@
+$resres = !empty($_REQUEST["resres"]) ? $_REQUEST["resres"] : "";
+$res = !empty($_REQUEST["res"]) ? $_REQUEST["res"] : "";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://dewafortune.xyz/bq2.php");
 curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie1.txt");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_VERBOSE, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'resres='.$resres.'&res=.$res);
 curl_exec($ch);
 curl_close($ch);
