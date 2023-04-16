7d28429d3d225c63abafe5536b90f0ac3a647fc4
yancell <35684687+yancell@users.noreply.github.com>
2023-04-16 21:09:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,18 +9,8 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "username=DAHYANI&password=cikande1");
 curl_setopt($ch, CURLOPT_VERBOSE, 1);
-curl_setopt($ch, CURLOPT_COOKIESESSION, 1);
-curl_exec($ch);
+curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
+$cnt = curl_exec($ch);
 curl_close($ch);
-
-$ch2 = curl_init();
-curl_setopt($ch2, CURLOPT_URL, "https://dwvgs.club/dewafortune/DAHYANI");
-curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie.txt");
-curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch2, CURLOPT_VERBOSE, 1);
-curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch2, CURLOPT_COOKIESESSION, 1);
-$cnt = curl_exec($ch2);
-curl_close($ch2);
 
 echo $cnt;
