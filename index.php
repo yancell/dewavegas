2a143f1dfd2c88a080cac0c71e167b3e96f77d0b
yancell <35684687+yancell@users.noreply.github.com>
2023-04-16 21:31:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://dwvgs.club/loginUser");
-curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie.txt");
-curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -19,4 +17,14 @@ foreach($matches[1] as $item) {
     parse_str($item, $cookie);
     $cookies = array_merge($cookies, $cookie);
 }
-print_r($cookies);
+setcookie('XSRF-TOKEN', $cookies['XSRF-TOKEN'], time() + (86400 * 30), "/");
+setcookie('dewavegas_session', $cookies['dewavegas_session'], time() + (86400 * 30), "/");
+
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, "https://dwvgs.club/dewafortune/DAHYANI");
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch2, CURLOPT_VERBOSE, 1);
+curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
+$cnt = curl_exec($ch2);
+curl_close($ch2);
+echo $cnt;
