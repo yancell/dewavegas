900573bda5a2dc9ace8eead7760bb852052f4751
yancell <35684687+yancell@users.noreply.github.com>
2023-04-18 05:56:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "username=DAHYANI&password=cikande1");
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
+
 curl_exec($ch);
 curl_close($ch);
 
@@ -15,7 +15,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://dwvgs.club/");
 curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
+
 curl_exec($ch);
 curl_close($ch);
 
@@ -25,7 +25,7 @@ curl_setopt($ch2, CURLOPT_NOBODY, 1);
 curl_setopt($ch2, CURLOPT_URL, "https://dwvgs.club/dewafortune/DAHYANI");
 curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie.txt");
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch2, CURLOPT_VERBOSE, 1);
+
 curl_exec($ch2);
 $info = curl_getinfo($ch2, CURLINFO_REDIRECT_URL);
 curl_close($ch2);
@@ -34,7 +34,7 @@ $ch3 = curl_init();
 curl_setopt($ch3, CURLOPT_URL, $info);
 curl_setopt($ch3, CURLOPT_COOKIEJAR, "cookie1.txt");
 curl_setopt($ch3, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch3, CURLOPT_VERBOSE, 1);
+
 curl_exec($ch3);
 curl_close($ch3);
 
@@ -42,7 +42,7 @@ $ch3 = curl_init();
 curl_setopt($ch3, CURLOPT_URL, "https://dewafortune.xyz/auth/select_game_v2.php");
 curl_setopt($ch3, CURLOPT_COOKIEFILE, "cookie1.txt");
 curl_setopt($ch3, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch3, CURLOPT_VERBOSE, 1);
+
 curl_exec($ch3);
 curl_close($ch3);
 
@@ -50,7 +50,7 @@ $ch4 = curl_init();
 curl_setopt($ch4, CURLOPT_URL, "https://dewafortune.xyz/var_api.php");
 curl_setopt($ch4, CURLOPT_COOKIEFILE, "cookie1.txt");
 curl_setopt($ch4, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch4, CURLOPT_VERBOSE, 1);
+
 curl_setopt($ch4, CURLOPT_FOLLOWLOCATION, 1);
 $cnt = curl_exec($ch4);
 curl_close($ch4);
