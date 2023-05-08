ce7028ecb35567035c512871275018f415f0321a
yancell <35684687+yancell@users.noreply.github.com>
2023-05-08 17:29:24 +0700

    Update wheal.php

--- wheal.php
+++ wheal.php
@@ -11,11 +11,11 @@ $var = [0, 10000,20000,35000, 0,50000, 75000, 100000, 250000, 50000,0,10000,2000
 $res = ["d1d813a48d99f0e102f7d0a1b9068001","48d6215903dff56238e52e8891380c8f","9f27410725ab8cc8854a2769c7a516b8","d487dd0b55dfcacdd920ccbdaeafa351","bda9643ac6601722a28f238714274da4","e90dfb84e30edf611e326eeb04d680de","61db47dac8aefe03fc67ee1b65ecd8f6","490a0db9793cd8cfae191676bbb860e5","9768feb3fdb1f267b06093bc572952dd","994ae1d9731cebe455aff211bcb25b93","d1d813a48d99f0e102f7d0a1b9068001","48d6215903dff56238e52e8891380c8f","9f27410725ab8cc8854a2769c7a516b8","d487dd0b55dfcacdd920ccbdaeafa351","bda9643ac6601722a28f238714274da4","e90dfb84e30edf611e326eeb04d680de","61db47dac8aefe03fc67ee1b65ecd8f6","490a0db9793cd8cfae191676bbb860e5","9768feb3fdb1f267b06093bc572952dd","994ae1d9731cebe455aff211bcb25b93"];
 $value = cut($cnt, 'klanklcnaklasdac:',',anim');
 $arr = json_decode($value);
-$target = 250000;
+$target = [75000, 100000, 250000];
 if (is_array($arr)){
 	foreach ($arr as $k => $v){
 		if (!empty($v)){
-			if ($var[$k] == $target){
+			if (in_array($var[$k], $target)){
 				echo $var[$k];
 				$ch = curl_init();
 				curl_setopt($ch, CURLOPT_URL, "https://dewafortune.xyz/bq2.php");
