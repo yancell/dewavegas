3a0657755ab3bee9dad257e57b93898054839b7d
yancell <35684687+yancell@users.noreply.github.com>
2023-05-08 16:08:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover">
-		<link rel="stylesheet" href="css/custom.css">
 	</head>
 	<body>
 		<div id="data"></div>
