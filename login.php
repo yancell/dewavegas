9dfc7a682475dd60b8d88338abe0547ac99d8272
yancell <35684687+yancell@users.noreply.github.com>
2023-05-08 13:58:09 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,14 +2,15 @@
 
 @unlink("cookie.txt");
 @unlink("cookie1.txt");
-
+$user = "DAHYANI";
+$pass = "cikande1";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://dwvgs.club/loginUser");
 curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie.txt");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "username=DAHYANI&password=cikande1");
+curl_setopt($ch, CURLOPT_POSTFIELDS, "username=".$user."&password=".$pass);
 curl_exec($ch);
 curl_close($ch);
 
@@ -23,7 +24,7 @@ curl_close($ch);
 $ch2 = curl_init();
 curl_setopt($ch2, CURLOPT_HEADER, 1);
 curl_setopt($ch2, CURLOPT_NOBODY, 1);
-curl_setopt($ch2, CURLOPT_URL, "https://dwvgs.club/dewafortune/DAHYANI");
+curl_setopt($ch2, CURLOPT_URL, "https://dwvgs.club/dewafortune/".$user);
 curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie.txt");
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
 curl_exec($ch2);
