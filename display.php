ee799f3d5c527eacd6350a3f585d23a57f68e184
yancell <35684687+yancell@users.noreply.github.com>
2023-05-12 10:53:47 +0700

    Add files via upload

--- display.php
+++ display.php
@@ -2,12 +2,12 @@
 
 	require_once("config.php");
 	
-	$ch1 = curl_init();
-	curl_setopt($ch1, CURLOPT_URL, "https://dewafortune.xyz/wheel.php");
-	curl_setopt($ch1, CURLOPT_COOKIEFILE, "cookie1".$user.".txt");
-	curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
-	$cnt = curl_exec($ch1);
-	curl_close($ch1);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://dewafortune.xyz/wheel.php");
+	curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie1".$user.".txt");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$cnt = curl_exec($ch);
+	curl_close($ch);
 	
 	$value = cut($cnt, 'klanklcnaklasdac:',',anim');
 	$arr = json_decode($value);
@@ -17,15 +17,15 @@
 			if (!empty($v)){
 				echo $var[$k];
 				if (in_array($var[$k], $target)){
-					$ch2 = curl_init();
-					curl_setopt($ch2, CURLOPT_URL, "https://dewafortune.xyz/bq2.php");
-					curl_setopt($ch2, CURLOPT_COOKIEFILE, "cookie1".$user.".txt");
-					curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-					curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
-					curl_setopt($ch2, CURLOPT_POST, 1);
-					curl_setopt($ch2, CURLOPT_POSTFIELDS, 'resres='.$res[$k].'&res='.$res[$k]);
-					curl_exec($ch2);
-					curl_close($ch2);
+					$ch = curl_init();
+					curl_setopt($ch, CURLOPT_URL, "https://dewafortune.xyz/bq2.php");
+					curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie1".$user.".txt");
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+					curl_setopt($ch, CURLOPT_POST, 1);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, 'resres='.$res[$k].'&res='.$res[$k]);
+					curl_exec($ch);
+					curl_close($ch);
 				}
 				break;
 			}
